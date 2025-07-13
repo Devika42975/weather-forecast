fa98be020a4d27039ca610321a32f9d6466c4a0e
Devika42975 <your-email@example.com>
2025-07-13 12:41:43 +0530

    Updates

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders city input', () => {
-  render(<App />);
-  const inputElement = screen.getByPlaceholderText(/enter city/i);
-  expect(inputElement).toBeInTheDocument();
-});
